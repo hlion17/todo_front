9c4ba831d0afe5f8502a84bff356fc5eb7d86aff
hlion17 <yourdad20626@gmail.com>
2023-10-14 23:51:54 +0900

    config: 로컬 개발환경 세팅

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+
+    // npm run build 타겟 디렉토리
+    outputDir: '../backend/src/main/resource/static',
+
+    // Backend proxy 설정
+    devServer: {
+        proxy: {
+            '/': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                ws: false,
+            }
+        }
+    },
+
 })
